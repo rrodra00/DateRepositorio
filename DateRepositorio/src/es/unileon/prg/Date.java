44fac9cc85ef68044b112a8ca7557394c591b193
rrodra00 <rrodra00@estudiantes.unileon.es>
2016-04-07 19:10:00 +0200

    Actualizados metodos faciles y añadidos algunos ejercicios for

--- DateRepositorio/src/es/unileon/prg/Date.java
+++ DateRepositorio/src/es/unileon/prg/Date.java
@@ -1,29 +1,60 @@
+import java.util.Scanner;
+
 //package es.unileon.prg;
 
 public class Date {
-	private int year, month, day;
+	private int day1, day2;
+	private int month1, month2;
+	private int year1, year2;
 	
+	/*
 	public Date(){
 		this.day=day;
-		this.month=motnh;
+		this.month=month;
 		this.year=year;
 	}
+	*/
+		
+	public void entrada(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca dia 1");
+		int day1 = sc.nextInt();
+		System.out.println("Introduzca mes 1");
+		int month1 = sc.nextInt();
+		System.out.println("Introduzca año 1");
+		int year1 = sc.nextInt();
+		System.out.println("Introduzca dia 2");
+		int day2 = sc.nextInt();
+		System.out.println("Introduzca mes 2");
+		int month2 = sc.nextInt();
+		System.out.println("Introduzca año 2");
+		int year2 = sc.nextInt();
+	}
 	
-	public boolean isSameYear(int year){
-		return null;
+	public boolean isSameYear(){
+		if(year1 == year2)
+		return true;
+		else
+			return false;
 	}
 	
 	public boolean isSameMonth(int month){
-		return null;
+		if(month1 == month2)
+		return true;
+		else
+			return false;
 	}
 
 	public boolean isSameDay(int day){
-		return null;
+		if(day1 == day2)
+		return true;
+		else
+			return false;
 	}
 	
 	public String getMonthName(){
 		String mes = new String (" ");
-		switch(month){
+		switch(month1){
 			case 1: mes = "Enero";
 			break;
 			case 2: mes = "Febrero";
@@ -52,45 +83,144 @@ public class Date {
 		return mes ;	
 	}
 	
-	public int checkDay(){
-		int mes = new int;
-		switch(month){
-		case 1|3:
-		day <= 31
-		break;
+	public boolean checkDay(){
+		switch(month1){
+		case 1:
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
 		case 2:
-		if(month = "Abril"|"Junio"|"Septiembre"|"Noviembre")
-		day <=30
-		break;
+			if(day1 <= 28)
+				return true;
+			else 
+				return false;
 		case 3:
-		if(month = "Febrero")
-		day <= 28
-		break;
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
+		case 4:
+			if(day1 <= 30)
+				return true;
+			else 
+				return false;
+		case 5:
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
+		case 6:
+			if(day1 <= 30)
+				return true;
+			else 
+				return false;
+		case 7:
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
+		case 8:
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
+		case 9:
+			if(day1 <= 30)
+				return true;
+			else 
+				return false;
+		case 10:
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
+		case 11:
+			if(day1 <= 30)
+				return true;
+			else 
+				return false;
+		case 12:
+			if(day1 <= 31)
+				return true;
+			else 
+				return false;
 		}
-	
+	return true;
 	}
 	
 	public String getSeasonMonth(){
 		String season = new String(" ");
-		switch(month){
-			case 1: 
-			if (month = "Enero"|"Febrero"||"Diciembre")
+		switch(month1){
+			case 1:
 			season = "Invierno";
 			break;
-			case 2: 
-			if (month == "Marzo"||"Abril"||"Mayo")
+			case 2:
+			season = "Invierno";
+			break;
+			case 3:
 			season = "Primavera";
 			break;
-			case 3: 
-			if (month == "Junio"||"Julio"||"Agosto")
+			case 4:
+			season = "Primavera";
+			break;
+			case 5:
+			season = "Primavera";
+			break;
+			case 6:
 			season = "Verano";
 			break;
-			case 4: 
-			if (month == "Septiembre"||"Octubre"||"Noviembre")
+			case 7:
+			season = "Verano";
+			break;
+			case 8:
+			season = "Verano";
+			break;
+			case 9:
 			season = "Otoño";
 			break;
+			case 10:
+			season = "Otoño";
+			break;
+			case 11:
+			season = "Otoño";
+			break;
+			case 12:
+			season = "Invierno";
+			break;	
 		}
-		return season;
+		return season;		
+	}
+	
+	public void imprimirDate(){
+		System.out.println(day1+"-"+month1+"-"+year1);
+	}
+	
+	public  void mesesRestantes(){
+		int aux = month1;
+		int contador=0;
+		for(int i=aux;i<12;i++){
+			contador++;
+		}
+		System.out.println("Quedan "+contador+" meses para final de año");
+	}
+
+	public void diasRestantesMes(){
+		int aux = day1;
+		int diaFinal;
+		if(month1==1|| month1==3||month1==5|| month1==7||month1==8|| month1==10||month1==12){
+			diaFinal=31;
+		}
+		if(month1==2){
+			diaFinal=28;
+		}
+		else{
+			diaFinal=30;
+		}
 		
+		for(int i=aux;i<diaFinal;i++){
+			System.out.println(aux+"-"+month1+"-"+year1);
+		}
+
 	}
 }
